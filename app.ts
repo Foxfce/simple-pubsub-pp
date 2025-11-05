98827ca0f9dd280aa1582d9a34b8386426d8a6c4
Foxfce <fenixz-1@hotmail.com>
2025-11-05 16:21:37 +0700

    fixed noti logic, add MachineRepo

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-type EventsType = "SoldEvent" | "RefillEvent" | "StockLowEvent" | "StockFineEvent" | "WarningEvent";
+type EventsType = "SoldEvent" | "RefillEvent" | "StockLowEvent" | "StockOkEvent";
 
 // interfaces
 interface IEvent {
@@ -7,7 +7,12 @@ interface IEvent {
 }
 
 interface ISubscriber {
-  handle(event: IEvent): void;
+  handle(
+    event: IEvent,
+    context: {
+      echoTo: (event: IEvent) => void
+    }
+  ): void;
 }
 
 interface IPublishSubscribeService {
@@ -20,10 +25,12 @@ interface IPublishSubscribeService {
 
 
 // implementations
-class InMemmoryPubSub implements IPublishSubscribeService {
-  private subscribed = new Map<EventsType, Set<ISubscriber>>(); // need option that not need Map
+class PubSubService implements IPublishSubscribeService {
+  private subscribed = new Map<EventsType, Set<ISubscriber>>();
   private queus: IEvent[] = [];
   private onProcess = false;
+
+  constructor(private machines: Machine[]) { }
 
   publish(event: IEvent): void {
     // Add new event to last of array
@@ -31,11 +38,10 @@ class InMemmoryPubSub implements IPublishSubscribeService {
 
     // If to prevent publish action while process is on going
     if (this.onProcess) return;
-
-    // Set ongoing state to true
     this.onProcess = true;
+
     try {
-      // check if their is action waiting in queus
+      // check if there is action waiting in queus
       while (this.queus.length > 0) {
         // Get first event in queus, ()! to tell ts that it will not be undefine
         const currentEvent = this.queus.shift()!;
@@ -45,11 +51,12 @@ class InMemmoryPubSub implements IPublishSubscribeService {
         if (!subscriberSet || subscriberSet.size === 0) continue; // no subscriber then jump to next event
 
         for (const sub of Array.from(subscriberSet)) {
-          sub.handle(event);
-          // sub.handle(current, {
-          //   machines: this.machineRef!,
-          //   publish: (e) => this.queus.push(e),
-          // });
+          sub.handle(
+            currentEvent, {
+            // machines: this.machines,
+            echoTo: (e: IEvent) => this.publish(e)
+          }
+          );
         }
       }
     }
@@ -59,17 +66,17 @@ class InMemmoryPubSub implements IPublishSubscribeService {
     }
   }
 
-  subscribe(topic: EventsType, subscribers: ISubscriber): void {
+  subscribe(eventType: EventsType, subscribers: ISubscriber): void {
     // create new if not existed, prevent error
-    if (!this.subscribed.has(topic)) {
-      this.subscribed.set(topic, new Set());
+    if (!this.subscribed.has(eventType)) {
+      this.subscribed.set(eventType, new Set());
     }
 
-    this.subscribed.get(topic)!.add(subscribers);
+    this.subscribed.get(eventType)!.add(subscribers);
   }
 
-  unsubscribe(topic: EventsType, subscribers: ISubscriber): void {
-    this.subscribed.get(topic)?.delete(subscribers)
+  unsubscribe(eventType: EventsType, subscribers: ISubscriber): void {
+    this.subscribed.get(eventType)?.delete(subscribers)
   }
 }
 
@@ -93,7 +100,7 @@ class MachineRefillEvent implements IEvent {
   constructor(private readonly _refill: number, private readonly _machineId: string) { }
 
   machineId(): string {
-    return this.machineId();
+    return this._machineId;
     // throw new Error("Method not implemented.");
   }
 
@@ -102,103 +109,277 @@ class MachineRefillEvent implements IEvent {
     // throw new Error("Method not implemented.");
   }
 
-  getRefillQuantity() : number{
+  getRefillQuantity(): number {
     return this._refill;
   }
 }
 
-class MachineSaleSubscriber implements ISubscriber {
-  public machines: Machine[];
+class LowStockWarningEvent implements IEvent {
+  constructor(private readonly _machineId: string, private readonly _stockLevel: number) { }
 
-  constructor(machines: Machine[]) {
-    this.machines = machines;
+  type(): EventsType {
+    return "StockLowEvent";
   }
 
-  handle(event: IEvent): void {
-    // if Event is not sale event stop handle function
-    if (!(event instanceof MachineSaleEvent)) return;
+  machineId(): string {
+    return this._machineId;
+  }
 
-    const machine = this.machines.find(x => x.id === event.machineId());
+  stockLevel(): number {
+    return this._stockLevel;
+  }
+}
+
+class StockLevelOkEvent implements IEvent {
+  constructor(private readonly _machineId: string, private readonly _stockLevel: number) { }
+
+  type(): EventsType {
+    return "StockOkEvent";
+  }
+
+  machineId(): string {
+    return this._machineId;
+  }
+
+  stockLevel(): number {
+    return this._stockLevel;
+  }
+}
+
+class MachineSaleSubscriber implements ISubscriber {
+  // public machines: Machine[];
+
+  // constructor(machines: Machine[]) {
+  //   this.machines = machines;
+  // }
+
+  constructor(private machineRepo: IMachineRepo) { }
+
+  handle(event: MachineSaleEvent, context: { echoTo: (event: IEvent) => void }): void {
+    const machine = this.machineRepo.findById(event.machineId());
     // can't find machine return
     if (!machine) return;
 
     const soldQty = event.getSoldQuantity();
-    machine.stockLevel = machine.stockLevel - soldQty;
 
-    console.log(`[SALE] ${machine.id} sold ${soldQty} cup${soldQty > 1 ? 's' : ''} , current stock is : ${machine.stockLevel}`);
-    // this.machines[2] still fixed with 2 need change later
+    if (machine.stockLevel < soldQty) {
+      console.log(`[ERROR] Machine${machine.id} Insufficient stock, current stock is : ${machine.stockLevel}`)
+      return;
+    }
+    machine.stockLevel -= soldQty;
+    this.machineRepo.update(machine);
+
+    console.log(`[SALE] Machine${machine.id} sold ${soldQty} cup${soldQty > 1 ? 's' : ''} , current stock is : ${machine.stockLevel}`);
+    if (machine.stockLevel < 3 && !machine.notiLowStock) {
+      machine.changeLowStockState(true);
+      this.machineRepo.update(machine);
+      context.echoTo(new LowStockWarningEvent(machine.id, machine.stockLevel));
+    }
+
     // this.machines[2].stockLevel -= event.getSoldQuantity();
   }
 }
 
 class MachineRefillSubscriber implements ISubscriber {
-  constructor(private machines: Machine[]) { }
+  // constructor(private machines: Machine[]) { };
+  constructor(private machineRepo: IMachineRepo) { }
+
+  handle(event: MachineRefillEvent, context: { echoTo: (event: IEvent) => void }): void {
+
+    const machine = this.machineRepo.findById(event.machineId());
+    if (!machine) {
+      console.log(`[ERROR] Machine${event.machineId()} does not existed`);
+      return;
+    };
+
+    const refillQty = event.getRefillQuantity();
+    machine.stockLevel += refillQty;
+    this.machineRepo.update(machine);
+
+
+    console.log(`[REFILL] Machine${machine.id} refilled ${refillQty} cup${refillQty > 1 ? 's' : ''} , current stock is : ${machine.stockLevel}`);
+
+    if (machine.stockLevel >= 3 && machine.notiLowStock) {
+
+      machine.changeLowStockState(false);
+      this.machineRepo.update(machine);
+
+      context.echoTo(new StockLevelOkEvent(machine.id, machine.stockLevel));
+    }
+
+    // throw new Error("Method not implemented.");
+  }
+}
+
+class StockWarningSubscriber implements ISubscriber {
+  // constructor(private currentMachine: Machine[]) { };
+  constructor(private machineRepo: IMachineRepo) { }
 
   handle(event: IEvent): void {
-    if (!(event instanceof MachineRefillEvent)) return;
+    const machine = this.machineRepo.findById(event.machineId());
 
-    const machine = this.machines.find( i => i.id === event.machineId());
-    if(!machine) return;
-
-    const refillQty = machine.stockLevel + event.getRefillQuantity();
-
-    console.log(`[REFILL] ${machine.id} refilled ${refillQty} cup${refillQty > 1 ? 's' : ''} , current stock is : ${machine.stockLevel}`);
-    // throw new Error("Method not implemented.");
+    if (event.type() === "StockLowEvent") {
+      console.log(`[WARNING] Machine${machine!.id} stock is LOW, current stock is : ${machine!.stockLevel}`);
+    } else if (event.type() === "StockOkEvent") {
+      console.log(`[OK] Machine${machine!.id} stock is now OK, current stock is : ${machine!.stockLevel}`);
+    }
   }
 }
 
 
 // objects
 class Machine {
-  public stockLevel = 10;
+  public stockLevel = 5;
   public id: string;
 
-  // for low stock noti check
-  public notiLowStock : boolean = false;
+  // for low stock noti check (no repeat)
+  public notiLowStock: boolean = false;
 
   constructor(id: string) {
     this.id = id;
+  }
+
+  changeLowStockState(state: boolean): void {
+    this.notiLowStock = state;
+  }
+}
+
+// Try in repository
+interface IMachineRepo {
+  getAll(): Machine[];
+  findById(id: string): Machine | undefined;
+  update(machine: Machine): void;
+  add(machines: Machine[]): void;
+  remove(machineId: string | undefined): void;
+}
+
+class MachineRepo implements IMachineRepo {
+  private machines = new Map<string, Machine>();
+
+  constructor(initialMachines: Machine[]) {
+    initialMachines.forEach(mech => this.machines.set(mech.id, mech));
+  }
+
+  getAll(): Machine[] {
+    return Array.from(this.machines.values())
+  }
+
+  findById(id: string): Machine | undefined {
+    return this.machines.get(id);
+  }
+
+  update(machine: Machine): void {
+    this.machines.set(machine.id, machine)
+  }
+
+  add(newMachines: Machine[]): void {
+    newMachines.forEach(mech => {
+      if (this.machines.has(mech.id)) {
+        console.log(`[ERROR] Machine${mech.id} Already exsited`);
+        return;
+      }
+      console.log(`[ADD] Machine${mech.id} to Machine Repository`)
+      this.machines.set(mech.id, mech)
+    });
+  }
+
+  remove(machineId: string): void {
+    if (this.machines.has(machineId)) {
+      this.machines.delete(machineId);
+      console.log(`[REMOVE] Machine${machineId} from Repo`);
+      return;
+    }
+    console.log(`Machine${machineId} Already Removed`);
   }
 }
 
 
 // helpers
-const randomMachine = (): string => {
-  const random = Math.random() * 3;
-  if (random < 1) {
-    return '001';
-  } else if (random < 2) {
-    return '002';
+const randomMachine = (maxNum: number): string => {
+  if (maxNum >= 1000 || maxNum < 1) {
+    throw new Error('Provided number is invalid : should be between 1 and 999');
   }
-  return '003';
+  const random = Math.ceil(Math.random() * Math.floor(maxNum));
 
+  const randomToStr: string = random.toString();
+
+  if (randomToStr.length > 2) {
+    return randomToStr;
+  } else if (randomToStr.length > 1) {
+    return `0${randomToStr}`;
+  }
+  return `00${randomToStr}`;
 }
 
-const eventGenerator = (): IEvent => {
+const eventGenerator = (conCurrentMachine: number): IEvent => {
   const random = Math.random();
   if (random < 0.5) {
     const saleQty = Math.random() < 0.5 ? 1 : 2; // 1 or 2
-    return new MachineSaleEvent(saleQty, randomMachine());
+    return new MachineSaleEvent(saleQty, randomMachine(conCurrentMachine));
   }
   const refillQty = Math.random() < 0.5 ? 3 : 5; // 3 or 5
-  return new MachineRefillEvent(refillQty, randomMachine());
+  return new MachineRefillEvent(refillQty, randomMachine(conCurrentMachine));
 }
 
 // program
 (async () => {
   // create 3 machines with a quantity of 10 stock
   const machines: Machine[] = [new Machine('001'), new Machine('002'), new Machine('003')];
+  const machineRepo: MachineRepo = new MachineRepo(machines);
+
+  const machine003 = machineRepo.getAll()[2];
 
   // create a machine sale event subscriber. inject the machines (all subscribers should do this)
-  const saleSubscriber = new MachineSaleSubscriber(machines);
-  const refillSubscriber = new MachineRefillSubscriber(machines);
+  const saleSubscriber = new MachineSaleSubscriber(machineRepo);
+  const refillSubscriber = new MachineRefillSubscriber(machineRepo);
+  const stockWarningSubscriber = new StockWarningSubscriber(machineRepo);
+
+  // const saleUnSubscriber = new MachineSaleSubscriber(machines);
 
   // create the PubSub service
-  const pubSubService: IPublishSubscribeService = new InMemmoryPubSub() // implement and fix this
+  const pubSubService = new PubSubService(machines) // implement and fix this
 
-  // create 5 random events
-  const events = [1, 2, 3, 4, 5].map(i => eventGenerator());
+  pubSubService.subscribe("RefillEvent", refillSubscriber);
+  pubSubService.subscribe("SoldEvent", saleSubscriber);
+  pubSubService.subscribe("StockLowEvent", stockWarningSubscriber);
+  pubSubService.subscribe("StockOkEvent", stockWarningSubscriber);
+
+  // create 40 random events
+  const events = new Array(40).fill(null).map(i => eventGenerator(3));
+
+  let randomRemove;
 
   // publish the events
-  events.map((machineEvent) => pubSubService.publish(machineEvent));
+  events.map((machineEvent) => {
+
+    randomRemove = Math.ceil(Math.random() * 100);
+    if (randomRemove < 20) {
+      //   pubSubService.unsubscribe("SoldEvent", saleSubscriber);
+      machineRepo.remove(machine003.id);
+      //   console.log("Machine try to unsubscribe SoldEvent");   
+    }
+
+    pubSubService.publish(machineEvent);
+
+  });
+  console.log('--------- finish action 1 -------------');
+
+  const newMachines: Machine[] = [new Machine('004'), new Machine('005')];
+
+  machineRepo.add(newMachines);
+
+  const events2 = new Array(40).fill(null).map(i => eventGenerator(5));
+
+  events2.map((machineEvent) => {
+
+    // random add machine003 back
+    randomRemove = Math.ceil(Math.random() * 100);
+    if (randomRemove < 10) {
+      machineRepo.add([machine003]);
+    }
+
+    pubSubService.publish(machineEvent);
+
+  });
+
 })();
